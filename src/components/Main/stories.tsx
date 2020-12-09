cab0247bd482937e3f432055b9dc70f184dbb7dc
Denner Rondinely <denner.rondinely@bizcap.com.br>
2020-12-08 22:33:32 -0300

    Add storybook

---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,16 @@
+import { withKnobs, text } from '@storybook/addon-knobs';
+import { Story, Meta } from '@storybook/react/types-6-0'
+import Main from './index';
+
+export default {
+  title: 'Main',
+  component: Main,
+  decorators: [withKnobs]
+} as Meta;
+
+export const Basic: Story = () => (
+  <Main
+    title={text('Title', 'React Avançado')}
+    description={text('Description', 'Typescript, ReactJS, NextJS e Styled Components')}
+  />
+);
